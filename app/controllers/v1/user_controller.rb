18461f0e5fcc45cb78b143f7ce7fd4955c9e7f10
Huguez <carlos.huguez@gmail.com>
2021-09-22 21:02:12 -0700

    inicio de la aplicacion y CRUD de usuarios

---
+++ app/controllers/v1/user_controller.rb
@@ -0,0 +1,89 @@
+class V1::UserController < ApplicationController
+    
+    rescue_from ActionController::ParameterMissing do |exception|
+
+        render :json => {:error => exception.message, :msg => "Faltaron los parametros" }, :status => 422
+    end
+
+    # POST /user/
+    def create
+        begin
+            @campos = [ "name", "email", "password", "role" ]
+            
+            @campos.each do |elem|
+                if params["user"][elem].nil?
+                    render :json => { :error => "parametros incompletos" }, :status => :bad_request
+                end
+            end
+            
+            @user = User.new( user_params )
+            if @user.save
+                # render :json => { :user => @user, :token => "wawa" }, :status => :created
+                render :json => { :user => @user }, :status => :created
+            else
+                render json: @user.error, status: :unprocessable_entity
+            end 
+        rescue => e
+            render json: {
+                error: e.to_s
+            }, status: :unprocessable_entity
+        end
+    end    
+    
+    # GET /user/
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+    
+    # GET /user/:id
+    def show
+        begin
+            @user = User.find(params[:id])
+            render json: @user, status: :ok
+        rescue ActiveRecord::RecordNotFound => e
+            render json: {
+                error: e.to_s
+            }, status: :not_found
+        end
+    end
+    
+    # PUT/PATH /user/:id
+    def update
+        begin
+            @user = User.find(params[:id])
+            if @user.update( user_params )
+                render json: @user, status: :ok
+            else
+                render json: @user.error, status: :unprocessable_entity
+            end
+        rescue ActiveRecord::RecordNotFound => e
+            render json: {
+                error: e.to_s
+            }, status: :not_found
+        end
+    end
+
+    # DELETE /user/:id
+    def destroy
+        begin
+            @user = User.find( params[:id] )
+            if @user.destroy
+                render json: @user, status: :ok
+            end
+        
+        rescue ActiveRecord::RecordNotFound => e
+            render json: {
+                error: e.to_s
+            }, status: :not_found
+        end
+    end
+
+    private
+
+        def user_params
+            # :name, :email, :password, :role 
+            params.require("user").permit( [ "name", "email", "password", "role" ] )
+        end
+
+end
